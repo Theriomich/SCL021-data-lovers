bdb704d6316ce894950235124b2f2c50cf9cfddf
Theriomich <theriomich@gmail.com>
2022-08-01 16:14:12 -0400

    Pusimos musica de fondo y segunda pagina

--- src/main.js
+++ src/main.js
@@ -1,6 +1,22 @@
 import { example } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+import data from './data/harrypotter/data.js';
 // import data from './data/rickandmorty/rickandmorty.js';
+
+const playSound = function() {
+    let element = document.createElement('div');
+    element.setAttribute('style', 'display: none');
+    element.innerHTML = `
+      <audio autoplay loop>
+        <source src="Harry Potter Theme Song.mp3" type="audio/mp3">
+    
+      </audio>
+    `;
+    document.body.appendChild(element);
+    document.removeEventListener('click', playSound);
+}
+
+document.addEventListener('click', playSound);
+
 
 console.log(example, data);
